81cf550c527e3ebcdeb45ef4a08767280319b9d0
Leon Lin <l84328g@gmail.com>
2024-02-25 23:20:32 +0800

    #2 feature: add data container.

---
+++ src/data_process/data_structure.py
@@ -0,0 +1,45 @@
+
+from dataclasses import dataclass
+from collections import deque
+
+
+@dataclass
+class BaseCoinMeta:
+    pass
+
+
+@dataclass
+class MACoinMeta:
+    pass
+
+
+
+@dataclass
+class CoinData:
+    raw_data: deque
+    data_pool: dict
+
+
+class MAMeta:
+    def __init__(self, symbol, maxlen, ma_range_list):
+        self.symbol = symbol
+        self.ticks = deque(maxlen=maxlen)
+        self.ma_data_pool = {}
+        self.signal_count = 0
+        self.ma_ranges = ma_range_list
+        for ma_range in ma_range_list:
+            self.ma_data_pool[ma_range] = deque(maxlen=maxlen)
+
+    def put_tick(self, tick):
+        self.ticks.append(tick)
+        cur_ticks_len = len(self.ticks)
+        for ma_range in self.ma_ranges:
+            if cur_ticks_len >= ma_range and cur_ticks_len > 1:
+                start = cur_ticks_len - ma_range
+                ma_range_data = list(self.ticks)[start:]
+                mean_average = sum(ma_range_data) / len(ma_range_data)
+                self.ma_data_pool[ma_range].append(mean_average)
+
+    @property
+    def isValid(self):
+        return len(self.ticks) >= max(self.ma_ranges)
