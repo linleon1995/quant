728a1df6dda22e5a09cb924130bbbed7a90473fd
Leon Lin <l84328g@gmail.com>
2024-02-27 01:58:49 +0800

    #2 add drawing function.

--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+
 
 def load_config():
     pass
@@ -12,3 +14,17 @@ def seconds_to_time_string(seconds):
     else:
         hours = seconds / 3600
         return f'{int(hours)}h'
+    
+def draw(symbol, timestamps, ticks, save_path):
+    # Plot the data
+    fig, ax = plt.subplots(1, 1)
+    ax.plot(timestamps, ticks, marker='o')
+    ax.set_xlabel('Time')
+    ax.set_ylabel('Values')
+    ax.set_title(f'{symbol} Values over Time')
+    ax.grid(True)
+    ax.tick_params(axis='x', rotation=45)  # Rotate x-axis labels for better readability
+
+    # Display the plot
+    fig.tight_layout()
+    fig.savefig(save_path)
