786187db4207b7e25dd73ec8d03f03fdc7cca29d
JingSiang <l84328g@gmail.com>
2024-05-08 00:29:09 +0800

    #7 feature: add the first backtest flow.

--- src/backtset.py
+++ src/backtset.py
@@ -1,13 +1,7 @@
-import time
-from datetime import datetime, timedelta
-import sys
-print(sys.path)
+from datetime import datetime
 
-import pandas as pd
-
-from binance_api import BinanceAPI
-from create_backtest_database import ArcticDBOperator
-from strategies import mean_average, peak
+from src.create_backtest_database import ArcticDBOperator
+from src.strategies import mean_average, base_strategy
 
 
 # TODO: 假設前提為無資金假設，所有的交易都可以被完成，考慮這種情況的策略表現
@@ -23,21 +17,27 @@ class Evaluator:
 
 
 def main():
+    symbol = 'WIFUSDT'
+
     arctic_ops = ArcticDBOperator(url="lmdb:///Leon/SideProject/quant", lib_name='Binance')
-    DBobject = arctic_ops.read(data_name='WIFUSDT', date_range=(datetime(2024, 4, 1, 0, 0), datetime(2024, 4, 15, 16, 17)))
+    DBobject = arctic_ops.read(data_name=symbol, date_range=(datetime(2024, 4, 1, 0, 0), datetime(2024, 4, 15, 16, 17)))
     evaluator = Evaluator()
-    ma_strategy = mean_average.Strategy(
-        ma_gap_rates=[1.008, 1.008],
-        ma_grow_rates=[1.0005, 1.0005, 1.0005],
-        count_threshold=5
+    strategy = base_strategy.TestStateMachine(
+        symbol=symbol,
+        maxlen=1440,
+        ma_range_list=[7, 25, 99]
     )
+    trader = None
+    wallet = None
     for tick in DBobject.data.iterrows():
         timestamp, data = tick
         close_price = data['close_price']
-        trade_singal = ma_strategy.run(close_price)
-        evaluator.collect(trade_singal, tick)
-        print(tick)
-    evaluator.eval()
+        trade_request = strategy.receive(price=close_price, 
+                                         timestamp=timestamp)
+        trade_response, trade_result = trader.trade(trade_request)
+        strategy.push_trade_response(trade_response)
+        wallet.push_trade_result(trade_result)
+    evaluator.eval(wallet.init_asset, wallet.trade_history)
 
 
 if __name__ == '__main__':
