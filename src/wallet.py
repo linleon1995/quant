a9fa1bf96e3029a6bee79bf1245c3ad96adffdfe
JingSiang <l84328g@gmail.com>
2024-06-14 02:12:30 +0800

    #5 feat: basic wallet class and testing.

--- src/wallet.py
+++ src/wallet.py
@@ -1,12 +1,72 @@
+from dataclasses import dataclass, field
+from typing import List, Dict
+from collections import deque
+from datetime import datetime
 
+
+@dataclass
+class Coin:
+    symbol: str
+    number: float = 0.0
+
+    def __add__(self, coin: 'Coin') -> 'Coin':
+        self.number = self.number + coin.number
+        return self
+
+@dataclass
+class Response:
+    status: str
+
+
+@dataclass
+class ActionStatus:
+    success: str = 'success'
+    fail: str = 'fail'
+    
+
+    
+@dataclass
+class TradeData:
+    action: str
+    number: float
+    altcoin_name: str
+    altcoin_price: float
+    bridgecoin_name: str = 'USDT'
+    bridgecoin_price: float = 1
+
+
+@dataclass
+class Asset:
+    coins: Dict[str, Coin] = field(default_factory=dict)
+
+    def deposit(self, coin: Coin):
+        symbol = self.coins.get(coin.symbol) 
+        if symbol is not None:
+            self.coins[symbol] = self.coins[symbol] + coin
+        else:
+            # setattr(self, coin.symbol, coin.number)
+            self.coins[coin.symbol] = coin
+
+    def get_coin(self, coin_name: str) -> Coin:
+        return self.coins.get(coin_name)
+    
+    def get_balance(self) -> dict:
+        return self.coins
 class BaseWallet:
     def __init__(self):
-        pass
+        self.asset = Asset()
 
-    def deposit(self, amount: float, coin_name: str) -> dict:
-        # type error
-        # number value range error
-        # coin error
-        pass
+    def deposit(self, coin: Coin) -> dict:
+        try:
+            self.asset.deposit(coin)
+            return Response(status=ActionStatus.success)
+        except Exception:
+            return Response(status=ActionStatus.fail)
         
-
+    def add_trade(self, trade_signal: TradeData):
+        if trade_signal.action == 'buy':
+            balance = self.asset.coins[trade_signal.bridgecoin_name].number - trade_signal.altcoin_price * trade_signal.number
+            if balance >= 0:
+                self.asset.coins[trade_signal.bridgecoin_name].number = balance
+                coin = Coin(symbol=trade_signal.altcoin_name, number=trade_signal.number)
+                self.asset.deposit(coin)
--- tests/test_wallet.py
+++ tests/test_wallet.py
@@ -1,17 +1,82 @@
 import pytest
 
+from src.wallet import BaseWallet, Coin, TradeData, Asset
 
+@pytest.fixture
 def mock_wallet():
+    return BaseWallet()
+
+@pytest.fixture
+def mock_asset():
+    return Asset()
+
+def test_coin():
+    c1 = Coin('USDT', 1000)
+    c2 = Coin('USDT', 2000)
+    c3 = c1 + c2
+    assert c1 is c3
+    assert c1 is not c2
+    assert c3.number == 3000
+
+
+# def test_withdraw(mock_wallet):
+#     mock_wallet.withdraw()
+
+def test_base_function(mock_wallet):
     pass
 
-def test_withdraw(mock_wallet):
-    mock_wallet.withdraw()
-
-
 def test_deposit(mock_wallet):
-    response = mock_wallet.deposit(100000, 'BTC')
-    assert response['status'] == 'success'
+    usdt = Coin(symbol='USDT', number=10000)
+    response = mock_wallet.deposit(usdt)
+    assert mock_wallet.asset.coins['USDT'] == usdt
+    assert response.status == 'success'
     
+def test_trade_new(mock_wallet):
+    usdt = Coin(symbol='USDT', number=100000)
+    response = mock_wallet.deposit(usdt)
+    eth = Coin(symbol='ETH', number=2)
+    response = mock_wallet.deposit(eth)
+    
+    trade_signal = TradeData(altcoin_name='ETH', altcoin_price=3000, 
+                             bridgecoin_name='USDT', bridgecoin_price=1, 
+                             action='buy', number=1.0)
+    trade_response = mock_wallet.add_trade(trade_signal)
+
+    assert trade_response.status == 'success'
+    assert mock_wallet.get_balance('USDT') == 97000
+    assert mock_wallet.get_balance('ETH') == 3
+    
+    trade_signal = TradeData(altcoin_name='BTC', altcoin_price=70000, 
+                             bridgecoin_name='USDT', bridgecoin_price=1, 
+                             action='sell', number=1.0)
+    trade_response = mock_wallet.add_trade(trade_signal)
+    assert trade_response.status == 'fail'
+    assert mock_wallet.get_balance('USDT') == 97000
+    assert mock_wallet.get_balance('ETH') == 3
+
+
+def test_asset_get_balance(mock_asset):
+    usdt = Coin(symbol='USDT', number=1000)
+    eth = Coin(symbol='ETH', number=1.2)
+    mock_asset.deposit(usdt)
+    mock_asset.deposit(eth)
+    balance = mock_asset.get_balance()
+    assert isinstance(balance, dict)
+    assert balance['USDT'].number == 1000
+    assert balance['ETH'].number == 1.2
+    
+    eth = Coin(symbol='ETH', number=1.3)
+    mock_asset.deposit(eth)
+    assert balance['ETH'].number == 2.5
+
+
+def test_asset_add_coin(mock_asset):
+    usdt = Coin(symbol='USDT', number=1000)
+    mock_asset.deposit(usdt)
+    usdt_coin = mock_asset.get_coin('USDT')
+    assert usdt_coin.number == 1000
+
+
 
 def test_trade(mock_wallet):
     response = mock_wallet.deposit(100000, 'BTC')
