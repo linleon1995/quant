099804bf87c0b8e97ca51de28843d7509c6bc1c2
Leonjs_lin <Leonjs_lin@asus.com>
2024-06-19 02:38:39 +0800

    #5 feature: complete the wallet feature and testing.

--- src/wallet.py
+++ src/wallet.py
@@ -16,6 +16,7 @@ class Coin:
 @dataclass
 class Response:
     status: str
+    msg: str = ''
 
 
 @dataclass
@@ -40,18 +41,45 @@ class Asset:
     coins: Dict[str, Coin] = field(default_factory=dict)
 
     def deposit(self, coin: Coin):
-        symbol = self.coins.get(coin.symbol) 
-        if symbol is not None:
-            self.coins[symbol] = self.coins[symbol] + coin
+        storeged_coin = self.coins.get(coin.symbol) 
+        if storeged_coin is not None:
+            self.coins[coin.symbol] = self.coins[coin.symbol] + coin
         else:
             # setattr(self, coin.symbol, coin.number)
             self.coins[coin.symbol] = coin
 
-    def get_coin(self, coin_name: str) -> Coin:
-        return self.coins.get(coin_name)
+    def withdraw(self, coin: Coin):
+        stored_coin = self.coins.get(coin.symbol)
+        ori_balance = stored_coin.number
+        if stored_coin is not None:
+            if stored_coin.number < coin.number:
+                raise ValueError(f'Not enough to withdraw {coin.number} {coin.symbol} for current balance {ori_balance}')
+            else:
+                stored_coin.number -= coin.number
+        else:
+            raise ValueError('No coin')
+        
+    def get_coin(self, symbol: str) -> Coin:
+        return self.coins.get(symbol)
     
+    def get_coin_balance(self, symbol: str) -> float:
+        coin = self.coins.get(symbol)
+        if coin is not None:
+            return coin.number
+        else:
+            return 0.0
+        
     def get_balance(self) -> dict:
         return self.coins
+    
+    def check_balance(self, symbol: str, number: int) -> bool:
+        stored_coin = self.coins.get(symbol)
+        if stored_coin is not None:
+            return stored_coin.number >= number
+        else:
+            return False
+    
+
 class BaseWallet:
     def __init__(self):
         self.asset = Asset()
@@ -63,10 +91,35 @@ class BaseWallet:
         except Exception:
             return Response(status=ActionStatus.fail)
         
+    def get_coin_balance(self, symbol: str) -> float:
+        coin = self.asset.coins.get(symbol)
+        if coin is not None:
+            return coin.number
+        else:
+            return 0.0
+        
     def add_trade(self, trade_signal: TradeData):
         if trade_signal.action == 'buy':
-            balance = self.asset.coins[trade_signal.bridgecoin_name].number - trade_signal.altcoin_price * trade_signal.number
-            if balance >= 0:
-                self.asset.coins[trade_signal.bridgecoin_name].number = balance
+            if self.asset.check_balance(symbol=trade_signal.bridgecoin_name, number=trade_signal.altcoin_price * trade_signal.number):
+                self.asset.withdraw(Coin(symbol=trade_signal.bridgecoin_name, number=trade_signal.altcoin_price * trade_signal.number))
                 coin = Coin(symbol=trade_signal.altcoin_name, number=trade_signal.number)
-                self.asset.deposit(coin)
+                try:
+                    self.asset.deposit(coin)
+                    return Response(status=ActionStatus.success)
+                except Exception as e:
+                    return Response(status=ActionStatus.fail)
+            else:
+                return Response(status=ActionStatus.fail)
+        elif trade_signal.action == 'sell':
+            if self.asset.check_balance(symbol=trade_signal.altcoin_name, number=trade_signal.number):
+                self.asset.withdraw(Coin(symbol=trade_signal.altcoin_name, number=trade_signal.number))
+                coin = Coin(symbol=trade_signal.bridgecoin_name, number=trade_signal.altcoin_price * trade_signal.number)
+                try:
+                    self.asset.deposit(coin)
+                    return Response(status=ActionStatus.success)
+                except Exception as e:
+                    return Response(status=ActionStatus.fail)
+            else:
+                return Response(status=ActionStatus.fail)
+        else:
+            raise ValueError('Invalid action')
--- tests/test_wallet.py
+++ tests/test_wallet.py
@@ -1,5 +1,8 @@
 import pytest
-
+import sys
+p = sys.path
+print(p)
+p.append('/home/linleon1995/project/quant/')
 from src.wallet import BaseWallet, Coin, TradeData, Asset
 
 @pytest.fixture
@@ -19,45 +22,61 @@ def test_coin():
     assert c3.number == 3000
 
 
-# def test_withdraw(mock_wallet):
-#     mock_wallet.withdraw()
-
-def test_base_function(mock_wallet):
-    pass
-
 def test_deposit(mock_wallet):
     usdt = Coin(symbol='USDT', number=10000)
     response = mock_wallet.deposit(usdt)
     assert mock_wallet.asset.coins['USDT'] == usdt
     assert response.status == 'success'
     
-def test_trade_new(mock_wallet):
+def test_trade(mock_wallet):
     usdt = Coin(symbol='USDT', number=100000)
     response = mock_wallet.deposit(usdt)
     eth = Coin(symbol='ETH', number=2)
     response = mock_wallet.deposit(eth)
     
+    # success
     trade_signal = TradeData(altcoin_name='ETH', altcoin_price=3000, 
                              bridgecoin_name='USDT', bridgecoin_price=1, 
                              action='buy', number=1.0)
     trade_response = mock_wallet.add_trade(trade_signal)
-
     assert trade_response.status == 'success'
-    assert mock_wallet.get_balance('USDT') == 97000
-    assert mock_wallet.get_balance('ETH') == 3
+    assert mock_wallet.get_coin_balance('USDT') == 97000
+    assert mock_wallet.get_coin_balance('ETH') == 3
     
+    # fail, not enough money
     trade_signal = TradeData(altcoin_name='BTC', altcoin_price=70000, 
                              bridgecoin_name='USDT', bridgecoin_price=1, 
                              action='sell', number=1.0)
     trade_response = mock_wallet.add_trade(trade_signal)
     assert trade_response.status == 'fail'
-    assert mock_wallet.get_balance('USDT') == 97000
-    assert mock_wallet.get_balance('ETH') == 3
+    assert mock_wallet.get_coin_balance('USDT') == 97000
+    assert mock_wallet.get_coin_balance('ETH') == 3
+
+    # fail, no coin
+    trade_signal = TradeData(altcoin_name='BNB', altcoin_price=3000, 
+                             bridgecoin_name='USDT', bridgecoin_price=1, 
+                             action='sell', number=4.0)
+    trade_response = mock_wallet.add_trade(trade_signal)
+    assert trade_response.status == 'fail'
+    assert mock_wallet.get_coin_balance('USDT') == 97000
+    assert mock_wallet.get_coin_balance('ETH') == 3
+    assert mock_wallet.get_coin_balance('BNB') == 0
+
+    # edge case
+    trade_signal = TradeData(altcoin_name='ETH', altcoin_price=4000,
+                             bridgecoin_name='USDT', bridgecoin_price=1, 
+                             action='sell', number=3.0)
+    trade_response = mock_wallet.add_trade(trade_signal)
+    assert trade_response.status == 'success'
+    assert mock_wallet.get_coin_balance('USDT') == 109000
+    assert mock_wallet.get_coin_balance('ETH') == 0
 
 
 def test_asset_get_balance(mock_asset):
     usdt = Coin(symbol='USDT', number=1000)
     eth = Coin(symbol='ETH', number=1.2)
+
+    # deposit, new coin
     mock_asset.deposit(usdt)
     mock_asset.deposit(eth)
     balance = mock_asset.get_balance()
@@ -65,56 +84,78 @@ def test_asset_get_balance(mock_asset):
     assert balance['USDT'].number == 1000
     assert balance['ETH'].number == 1.2
     
+    # deposit, add to existing coin
     eth = Coin(symbol='ETH', number=1.3)
     mock_asset.deposit(eth)
-    assert balance['ETH'].number == 2.5
+    assert mock_asset.get_coin_balance(symbol='ETH') == 2.5
+
+    # withdraw, subtract from existing coin
+    eth = Coin(symbol='ETH', number=1.0)
+    mock_asset.withdraw(eth)
+    assert mock_asset.get_coin_balance(symbol='ETH') == 1.5
+
+    # withdraw, not enough coin
+    eth = Coin(symbol='ETH', number=2)
+    try:
+        mock_asset.withdraw(eth)
+    except ValueError as e:
+        assert mock_asset.get_coin_balance(symbol='ETH') == 1.5
+
+    # withdraw, just enough coin
+    eth = Coin(symbol='ETH', number=1.5)
+    mock_asset.withdraw(eth)
+    assert mock_asset.get_coin_balance(symbol='ETH') == 0.0
 
 
 def test_asset_add_coin(mock_asset):
     usdt = Coin(symbol='USDT', number=1000)
     mock_asset.deposit(usdt)
     usdt_coin = mock_asset.get_coin('USDT')
+    assert usdt_coin.symbol == 'USDT'
     assert usdt_coin.number == 1000
 
+    usdt_number = mock_asset.get_coin_balance('USDT')
+    assert usdt_number == 1000
 
 
-def test_trade(mock_wallet):
-    response = mock_wallet.deposit(100000, 'BTC')
-    assert mock_wallet.total_value == 100000
+
+# def test_trade(mock_wallet):
+#     response = mock_wallet.deposit(100000, 'BTC')
+#     assert mock_wallet.total_value == 100000
  
-    assert mock_wallet.amount('BTC') is None
-    mock_wallet.buy(1, 'BTC', 'USDT', 70000)
-    assert mock_wallet.amount('BTC') == 1
-    assert mock_wallet.value('BTC') == 70000
-    assert mock_wallet.amount('USDT') == 30000
-    assert mock_wallet.value('USDT') == 1
+#     assert mock_wallet.amount('BTC') is None
+#     mock_wallet.buy(1, 'BTC', 'USDT', 70000)
+#     assert mock_wallet.amount('BTC') == 1
+#     assert mock_wallet.value('BTC') == 70000
+#     assert mock_wallet.amount('USDT') == 30000
+#     assert mock_wallet.value('USDT') == 1
 
-    assert mock_wallet.total_value == 100000
-    assert isinstance(mock_wallet.portfolio, dict)
-    # assert mock_wallet.portfolio.
-    assert mock_wallet.profit == 0
+#     assert mock_wallet.total_value == 100000
+#     assert isinstance(mock_wallet.portfolio, dict)
+#     # assert mock_wallet.portfolio.
+#     assert mock_wallet.profit == 0
     
-    assert mock_wallet.roi == 0
-    assert mock_wallet.profit == 0
+#     assert mock_wallet.roi == 0
+#     assert mock_wallet.profit == 0
 
-    mock_wallet.sell(0.5, 'BTC', 'USDT', 80000)
-    assert mock_wallet.amount('BTC') == 0.5
-    assert mock_wallet.price('BTC') == 80000
-    assert mock_wallet.value('BTC') == 40000
-    assert mock_wallet.amount('USDT') == 70000
-    assert mock_wallet.price('USDT') == 1
-    assert mock_wallet.value('USDT') == 70000
-    assert mock_wallet.roi == 0.1
-    assert mock_wallet.profit == 10000
-    assert mock_wallet.coin_profit('BTC') == 5000
-    assert mock_wallet.coin_roi('BTC') == 1/7
+#     mock_wallet.sell(0.5, 'BTC', 'USDT', 80000)
+#     assert mock_wallet.amount('BTC') == 0.5
+#     assert mock_wallet.price('BTC') == 80000
+#     assert mock_wallet.value('BTC') == 40000
+#     assert mock_wallet.amount('USDT') == 70000
+#     assert mock_wallet.price('USDT') == 1
+#     assert mock_wallet.value('USDT') == 70000
+#     assert mock_wallet.roi == 0.1
+#     assert mock_wallet.profit == 10000
+#     assert mock_wallet.coin_profit('BTC') == 5000
+#     assert mock_wallet.coin_roi('BTC') == 1/7
 
-    mock_wallet.update_price('BTCUSDT', 90000)
-    assert mock_wallet.roi == 0.15
-    assert mock_wallet.profit == 15000
-    assert mock_wallet.coin_profit('BTC') == 10000
-    assert mock_wallet.coin_roi('BTC') == 2/7
+#     mock_wallet.update_price('BTCUSDT', 90000)
+#     assert mock_wallet.roi == 0.15
+#     assert mock_wallet.profit == 15000
+#     assert mock_wallet.coin_profit('BTC') == 10000
+#     assert mock_wallet.coin_roi('BTC') == 2/7
 
-    # cost?
-    # update_multi_price
+#     # cost?
+#     # update_multi_price
 
