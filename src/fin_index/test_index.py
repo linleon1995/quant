2aef1fbd34d8abf222935c32a7d45859ac11e875
Leon <l84328g@gmail.com>
2024-06-26 02:04:44 +0800

    #10 fix: aviod wrong execution for unit test.

--- src/fin_index/test_index.py
+++ src/fin_index/test_index.py
@@ -3,50 +3,52 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import time
 
-# 设置随机种子，以便结果可重复
-np.random.seed(2)
 
-# 模拟的时间步长
-num_steps = 100
+if __name__ == '__main__':
+    # 设置随机种子，以便结果可重复
+    np.random.seed(2)
 
-# 生成时间序列
-time_range = np.arange(num_steps)
+    # 模拟的时间步长
+    num_steps = 100
 
-# 使用随机游走模型生成股价数据
-initial_price = 100
-drift = 0.05  # 漂移率
-volatility = 0.2  # 波动率
+    # 生成时间序列
+    time_range = np.arange(num_steps)
 
-# 使用几何布朗运动模型生成股价数据
-returns = np.random.normal((drift / num_steps), volatility, num_steps)
-price = initial_price * np.exp(np.cumsum(returns))
+    # 使用随机游走模型生成股价数据
+    initial_price = 100
+    drift = 0.05  # 漂移率
+    volatility = 0.2  # 波动率
 
-# 转换为 pandas Series 对象
+    # 使用几何布朗运动模型生成股价数据
+    returns = np.random.normal((drift / num_steps), volatility, num_steps)
+    price = initial_price * np.exp(np.cumsum(returns))
 
-st= time.time()
+    # 转换为 pandas Series 对象
 
-for _ in range(100):
-    price_series = pd.Series(price)
-    # 计算移动标准差
-    window_size = 20
-    moving_std = price_series.rolling(window=window_size).std()
-    # 计算布林通道的上轨和下轨
-    moving_average = price_series.rolling(window=window_size).mean()
-    upper_bound = moving_average + 2 * moving_std
-    lower_bound = moving_average - 2 * moving_std
+    st= time.time()
 
-elapsed = time.time() - st
-print(elapsed)
+    for _ in range(100):
+        price_series = pd.Series(price)
+        # 计算移动标准差
+        window_size = 20
+        moving_std = price_series.rolling(window=window_size).std()
+        # 计算布林通道的上轨和下轨
+        moving_average = price_series.rolling(window=window_size).mean()
+        upper_bound = moving_average + 2 * moving_std
+        lower_bound = moving_average - 2 * moving_std
 
-# 绘制股价图和布林通道
-plt.plot(time_range, price, label='Stock Price')
-plt.plot(time_range, moving_average, label='Moving Average')
-plt.plot(time_range, upper_bound, 'r--', label='Upper Bound')
-plt.plot(time_range, lower_bound, 'g--', label='Lower Bound')
+    elapsed = time.time() - st
+    print(elapsed)
 
-plt.xlabel('Time')
-plt.ylabel('Price')
-plt.title('Simulated Stock Price with Bollinger Bands')
-plt.legend()
-plt.grid(True)
-plt.show()
+    # 绘制股价图和布林通道
+    plt.plot(time_range, price, label='Stock Price')
+    plt.plot(time_range, moving_average, label='Moving Average')
+    plt.plot(time_range, upper_bound, 'r--', label='Upper Bound')
+    plt.plot(time_range, lower_bound, 'g--', label='Lower Bound')
+
+    plt.xlabel('Time')
+    plt.ylabel('Price')
+    plt.title('Simulated Stock Price with Bollinger Bands')
+    plt.legend()
+    plt.grid(True)
+    plt.show()
