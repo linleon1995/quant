d9f05dd3de865da5aa43f402aeb3edd9c6eaa7aa
Leon <l84328g@gmail.com>
2025-04-29 22:36:57 +0800

    #23 feat: change format klines.

--- write_binance_data.py
+++ write_binance_data.py
@@ -12,14 +12,14 @@ from src.data_process.data_structure import BinanceTick
 
 
 # TODO: complete the input, output, and testing
-def format_kline_data(data):
+def format_kline_data(data, tick_data):
     if data is not None or len(data) != 0:
-        tick_fields = [field.name for field in fields(class_or_instance=BinanceTick)]
+        tick_fields = [field.name for field in fields(class_or_instance=tick_data)]
         data = pd.DataFrame(data=data, columns=tick_fields)
-        start_timestamp_ms = data.iloc[0]['open_time']
-        start_timestamp_sec = start_timestamp_ms / 1000
-        end_timestamp_ms= data.iloc[-1]['open_time']
-        end_timestamp_ms = end_timestamp_ms / 1000
+        # start_timestamp_ms = data.iloc[0]['open_time']
+        # start_timestamp_sec = start_timestamp_ms / 1000
+        # end_timestamp_ms= data.iloc[-1]['open_time']
+        # end_timestamp_ms = end_timestamp_ms / 1000
 
         data["open_datetime"] = pd.to_datetime(data["open_time"], unit="ms")
         data = data.set_index(keys="open_datetime")
@@ -39,7 +39,11 @@ def write_coin_kline_into_database(binance_api, arctic_ops, symbol, start_time, 
 
     current_date = start_time
     print(f'{current_date} - [{symbol}] - second - {start_time} -> {end_time}')
+    last_time = None
     while current_date <= end_time:
+        if last_time != current_date.year:
+            last_time = current_date.year
+            print(f'{current_date} - [{symbol}] - {last_time}')
         st = time.time()
         start_time_in_day = int(current_date.timestamp()) * 1000
         binance_date_data = binance_api.get_klines(symbol=symbol, startTime=start_time_in_day)
@@ -47,12 +51,12 @@ def write_coin_kline_into_database(binance_api, arctic_ops, symbol, start_time, 
         if binance_date_data is None or len(binance_date_data) == 0:
             break
         
-        formated_binance_date_data = format_kline_data(binance_date_data)
+        formated_binance_date_data = format_kline_data(binance_date_data, tick_data=BinanceTick)
         arctic_ops.add(symbol, formated_binance_date_data)
 
         current_date += timedelta(minutes=len(formated_binance_date_data))
         elapsed_time = time.time() - st
-        print(f'[{symbol}] - {current_date} - elapsed time: {elapsed_time:.2f} s')
+        # print(f'[{symbol}] - {current_date} - elapsed time: {elapsed_time:.2f} s')
     print(f'[{symbol}] - finish')
 
 
@@ -61,10 +65,13 @@ def main():
         'ADAUSDT', 'WIFUSDT', 'ATAUSDT', 'PEPEUSDT', 'ETHUSDT', 'BTCUSDT', 'BNBUSDT', 'NEIROUSDT', 
         'PNUTUSDT', 'DOGEUSDT', 'TRUMPUSDT'
     ]
+    # old_coin_list = ['KEEPUSDT', 'LTOUSDT', 'PROUSDT', 'TRIBUSDT', 'SYNUSDT', 
+    # 'PENDLEUSDT', 'KNCUSDT', 'REPUSDT', 'POLUSDT', 'STXUSDT', 'IDUSDT', 'FTMISDT', 'BNTUSDT', 'ONEUSDT']
     binance_api = BinanceAPI()
     coin_list = binance_api.get_symbols()
-    coin_list = [symbol for symbol in coin_list if 'USDT' in symbol and 'UP' not in symbol and 'DOWN' not in symbol]
+    coin_list = [symbol for symbol in coin_list if symbol.endswith('USDT')  and 'UP' not in symbol and 'DOWN' not in symbol]
     coin_list = set(coin_list) - set(old_coin_list)
+    coin_list = ['PEPEUSDT']
     # ticker_prices = binance_api.get_usdt_ticker(bridge='USDT')
     # coin_list2 = [coin['symbol'] for coin in ticker_prices]
 
